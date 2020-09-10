4a34d8074e1134f0e7f053e31770b79b86b86ec0
paramveersoni1 <paramveer.tr.mdb@gmail.com>
2020-09-10 14:23:36 +0530

    responsive.css

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,5 +4,22 @@
         dots:true,
         autoplay: true,
         slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+            }
+          },
+          // {
+          //   breakpoint: 480,
+          //   settings: {
+          //     arrows: false,
+          //     centerMode: true,
+          //     centerPadding: '40px',
+          //     slidesToShow: 1
+          //   }
+          // }
+        ]
       });
